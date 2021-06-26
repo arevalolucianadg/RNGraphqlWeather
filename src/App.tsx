7759974ec402dee84800047258827750c7abbd2a
Luciana <arevalolucianadg@gmail.com>
2021-06-26 18:57:53 -0300

    add initial Storybook settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import { ThemeProvider } from 'styled-components';
+import StorybookUI from '../storybook';
+import {ThemeProvider} from 'styled-components';
 
 import Router from './routes';
 import {navigationRef} from './routes/RootNavigation';
-import { light } from './styles/theme';
+import {light} from './styles/theme';
+
+import {LOAD_STORYBOOK} from '@env';
 
 const App = () => {
   return (
     <ThemeProvider theme={light}>
-    <NavigationContainer ref={navigationRef}>
-      <Router />
-    </NavigationContainer>
+      <NavigationContainer ref={navigationRef}>
+        <Router />
+      </NavigationContainer>
     </ThemeProvider>
   );
 };
 
-export default App;
+export default LOAD_STORYBOOK === 'true' ? StorybookUI : App;
---
+++ storybook/storyLoader.js
@@ -0,0 +1,15 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require('../src/components/BottomTabItem/BottomTabItem.stories');
+}
+
+const stories = ['../src/components/BottomTabItem/BottomTabItem.stories'];
+
+module.exports = {
+  loadStories,
+  stories,
+};
