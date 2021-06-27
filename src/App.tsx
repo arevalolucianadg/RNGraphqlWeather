7a7160d7ea859ff34194e2c682177dc37819461a
Luciana <arevalolucianadg@gmail.com>
2021-06-27 16:44:35 -0300

    add ApolloClient config

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import StorybookUI from '../storybook';
 import {ThemeProvider} from 'styled-components';
+import {ApolloProvider} from '@apollo/client';
 
 import Router from './routes';
 import {navigationRef} from './routes/RootNavigation';
+import apolloConfig from './config/apollo';
+import StorybookUI from '../storybook';
 import {dark, light} from './styles/theme';
 
 import {LOAD_STORYBOOK} from '@env';
 
 const App = () => {
-
   return (
-    <ThemeProvider theme={light}>
-      <NavigationContainer ref={navigationRef}>
-        <Router />
-      </NavigationContainer>
-    </ThemeProvider>
+    <ApolloProvider client={apolloConfig}>
+      <ThemeProvider theme={light}>
+        <NavigationContainer ref={navigationRef}>
+          <Router />
+        </NavigationContainer>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 
