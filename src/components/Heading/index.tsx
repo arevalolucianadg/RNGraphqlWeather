0ce967def70fbcfc8d25ac0818de087ae0c401ed
luciana.arevalo <luciana.arevalo@ank.app>
2022-01-05 01:14:07 -0300

    chore: fix eslint conflicts heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 
-import {TitleText} from './Heading.styles';
+import {TitleText} from './styles';
 
+/**
+ * Types
+ */
 interface HeadingProps {
   primary?: boolean;
   children: string;
 }
 
-const Heading = ({primary, children}: HeadingProps) => {
-  return <TitleText primary={primary}>{children}</TitleText>;
-};
+const Heading: FunctionComponent<HeadingProps> = ({primary, children}) => ( 
+  <TitleText primary={primary}>{children}</TitleText>
+);
 
 export default Heading;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,7 @@
 export {default as BottomTabBar} from './BottomTabBar';
 export {default as BottomTabItem} from './BottomTabItem';
 export {default as Button} from './Button';
-export {default as Heading} from './Heading';
+export {default as Heading} from './heading';
 export {default as LayoutBase} from './layout-base';
 export {default as LoadingView} from './loading-view';
 export {default as NoResults} from './no-results';
