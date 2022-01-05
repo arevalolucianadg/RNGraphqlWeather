4af6a5f0721071ecba51b7309fee20adf71bab0f
luciana.arevalo <luciana.arevalo@ank.app>
2022-01-04 23:27:16 -0300

    chore: fix eslint conflicts home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,21 @@
-import React, {useCallback, useContext, useState} from 'react';
-import {FlatList, ListRenderItemInfo, RefreshControl, Text} from 'react-native';
+import React, {FunctionComponent, useContext, useState} from 'react';
 import {useQuery} from '@apollo/client';
+import {FlatList, ListRenderItemInfo, RefreshControl, Text} from 'react-native';
 
-import {AppContext} from '../../Context/AppContext/AppContext';
-import {GET_WEATHER_INFO} from '../../graphql/requests';
-import {CitiesInfo, QueryVars, WeatherInfo} from '../../graphql/interfaces';
 import {Heading, LayoutBase, LoadingView, WeatherCard} from '../../components';
 import {LayoutSpacing} from '../../components/LayoutBase/LayoutBase.styles';
-import {TitleWrapper, CitiesList} from './styles';
+import {AppContext} from '../../Context/AppContext/AppContext';
+import {CitiesInfo, QueryVars, WeatherInfo} from '../../graphql/interfaces';
+import {GET_WEATHER_INFO} from '../../graphql/requests';
 import { wait } from '../../utils/global';
+import {TitleWrapper, CitiesList} from './styles';
+
+/**
+ * Constants
+ */
+const STYLES_LIST = {
+  flex: 1
+}
 
 export interface HomeCityProps {
   id: string;
@@ -19,13 +26,11 @@ export interface HomeCityProps {
   temperature: string;
 }
 
-const Home = () => {
+const Home: FunctionComponent = () => {
   const {cities, favoriteCities, temperatureUnit} = useContext(AppContext);
   const [refreshing, setRefreshing] = useState(false);
 
-  const isFavorite = (city: ListRenderItemInfo<WeatherInfo>) => {
-    return favoriteCities.includes(city.item.id);
-  };
+  const isFavorite = (city: ListRenderItemInfo<WeatherInfo>): boolean => favoriteCities.includes(city.item.id);
 
   const variables = {
     id: cities,
@@ -36,13 +41,16 @@ const Home = () => {
 
   const {data, loading, error, refetch} = useQuery<CitiesInfo, QueryVars>(
     GET_WEATHER_INFO,
-    {variables, fetchPolicy: 'no-cache'},
+    {
+      variables, 
+      fetchPolicy: 'no-cache'
+    },
   );
 
   if (loading) return <LoadingView />;
   if (error) return <Text>Ocurrió un error.</Text>;
 
-  const onRefresh = () => {
+  const onRefresh = (): void => {
     setRefreshing(true);
     refetch();
     wait(2000).then(() => setRefreshing(false));
@@ -54,7 +62,7 @@ const Home = () => {
         <CitiesList>
           <FlatList
             data={data?.getCityById}
-            style={{flex: 1}}
+            style={STYLES_LIST}
             keyExtractor={city => city.id}
             renderItem={city => (
               <WeatherCard city={city.item} isFavorite={isFavorite(city)} />
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,4 +1,4 @@
-export {default as Home} from './Home';
+export {default as Home} from './home';
 export {default as Search} from './search';
 export {default as Settings} from './settings';
 export {default as WeatherDetail} from './weather-detail';
